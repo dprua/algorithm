4d9c43f6a6b95b23e36141444a91dc9ec5fe5eb7
dprua <34247631+dprua@users.noreply.github.com>
2020-05-18 11:40:04 +0900

    Create bubblesort.cpp
    Bubble_Sort

---
+++ bubblesort.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <iostream>
+
+//int main() {
+//	int i, j, temp;
+//	int arr[10] = { 1,2,5,8,7,6,4,3,10,9 };
+//
+//	for (i = 0; i < 10; i++) {
+//		for (j = 0; j < 9-i; j++) {
+//			if (arr[j] > arr[j + 1]) {
+//				temp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = temp;
+//			}
+//		}
+//	}
+//	for (i = 0; i < 10; i++) {
+//		printf("%d, ", arr[i]);
+//	}
+//}
+//int main() {
+//	int arr[10] = { 10,2,5,8,7,6,4,3,11,9 };
+//	int i, j, temp, max;
+//	for (i = 0; i < 10; i++) {		
+//		for (j = 1; j < 10 - i; j++) {
+//			if (arr[j] < arr[j-1]) {
+//				temp = arr[j];
+//				arr[j] = arr[j - 1];
+//				arr[j - 1] = temp;
+//			}
+//		}
+//	}
+//	for (i = 0; i < 10; i++) {
+//		printf("%d, ", arr[i]);
+//	}
+//}
