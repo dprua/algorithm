7739aa5068b99bd41b1785f4306a2c0f67a5b6d1
dprua <34247631+dprua@users.noreply.github.com>
2020-05-18 23:52:47 +0900

    Create Union-Find(합집합 찾기 알고리즘).cpp

---
+++ Union-Find(합집합 찾기 알고리즘).cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+//Union-Find(합집합 찾기 알고리즘)
+
+int getParent(int* parent, int x){  //부모의 값을 return해주는 함수
+	if (parent[x] == x) return x;	//자식값과 부모값이 같으면 해당 값을 return
+	return parent[x] = getParent(parent, parent[x]);  //같지 않으면 그 부모값을 가지고 그 부모의 부모값을 찾으러감.
+}
+
+void unionParent(int* parent, int a, int b) {	//두 노드를 연결해주는 함수
+	a = getParent(parent, a);	//a의 부모값을 가져와라
+	b = getParent(parent, b);	//b의 부모값을 가져와라
+	if (a < b)			//a의 부모값이 더 적으면
+		parent[b] = a;  //b의 부모값을 a로
+	else                //반대
+		parent[a] = b;
+}
+
+int compareParent(int* parent, int a, int b) {	//두 노드의 부모의 값이 같으면 1 다르면 0 반환
+	a = getParent(parent, a);	//a의 부모값을 가져와라
+	b = getParent(parent, b);	//b의 부모값을 가져와라
+	if (a == b)
+		return 1;
+	else
+		return 0;
+}
+int main() {
+	int parent[11];
+	int i;
+
+	for (i = 1; i <= 10; i++) {
+		parent[i] = i;
+	}
+
+	unionParent(parent, 1, 2);
+	unionParent(parent, 2, 3);
+	unionParent(parent, 3, 4);
+	unionParent(parent, 5, 6);
+	unionParent(parent, 6, 7);
+	unionParent(parent, 7, 8);
+	
+	printf("1과 2는 연결되어 있나요? : %d\n", compareParent(parent, 1, 2));
+	printf("1과 5는 연결되어 있나요? : %d\n", compareParent(parent, 1, 5));
+
+	unionParent(parent, 4, 5);
+
+	printf("1과 5는 연결되어 있나요? : %d\n", compareParent(parent, 1, 5));
+}
