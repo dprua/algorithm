055fc67e2548950306e7ccbaba84d5b1225ebaed
dprua <dprua@naver.com>
2021-09-15 16:27:58 +0900

    leetcode Palindrome linked list 판별(python,list[::-1])

--- leetcode/Palindrome Linked List/code.py
+++ leetcode/Palindrome Linked List/code.py
@@ -1,3 +1,18 @@
-result = [1,2,2,1]
-print(result)
-print(result[::-1])
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        
+        result = []
+        
+        while head != None:
+            value = head.val
+            head = head.next
+            result.append(value)
+        
+        if result == result[::-1]:
+            return True
+        return False
