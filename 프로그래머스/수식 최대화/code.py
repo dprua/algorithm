249d4f81014c275a635b97606ff018f99d08b32d
dprua <dprua@naver.com>
2021-10-07 17:22:15 +0900

    프로그래머스 수식 최대화(카카오 인턴, python, itertools(permutations,조합))

--- 프로그래머스/수식 최대화/code.py
+++ 프로그래머스/수식 최대화/code.py
@@ -1,3 +1,16 @@
+'''
+-*- coding: utf-8 -*-
+
+내 코딩 실력이 너무 고전적인 스타일 이라는 것을 많이 느끼게 한 문제이다.
+permutations 함수도 잘 몰라서 구글링을 통해 알아내서 활용할 수 있었다.
+꾸역꾸역 풀기는 했는데 코드가 정말 마음에 들지 않는다.
+나중에 다시 풀어볼 것이다. 
+풀이과정은 현재 expression에 등장한 operator를 알아내서 이 operator로 만들 수 있는 조합을
+permutations 함수를 통해 구한다.
+본 문제는 모든 경우의 수를 탐색 및 비교해야하는 완전탐색 문제이다. 
+따라서 구해진 조합의 순서대로 for문을 통해 전처리된 expression에서 우선순위에 맞게 연산을 수행한다.
+'''
+
 from itertools import permutations
 
 def calc(operators,expression):
@@ -8,7 +21,6 @@ def calc(operators,expression):
                 while True:
                     try:
                         idx = expression.index(op)
-                        idx =  expression.index(op)
                         num1 = expression.pop(idx-1)
                         operator = expression.pop(idx-1)
                         num2 = expression.pop(idx-1)
